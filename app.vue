59624a5e3cd973fc77d3b70313e44ba08baa7be2
Ron Pelt <pelt8@hotmail.com>
2024-04-06 22:35:13 +0200

    configure leaflet, add tailwind, proto locations added

--- app.vue
+++ app.vue
@@ -1,5 +1,50 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex flex-col md:flex-row bg-slate-800">
+    <div class="w-96 h-32 md:h-full p-4 ">
+      <h1 class="text-xl font-bold mb-4 text-white">Tabletop Locator</h1>
+      <input type="text" placeholder="Search..." class="border-2 p-1 w-full rounded-md">
+    </div>
+    <div class="w-[100vw] h-[100vh]" id="map">
+    </div>
   </div>
 </template>
+
+<script setup>
+  import { ref, onMounted } from 'vue';
+  import LocationsNL from './assets/locations/nl/locations.json';
+  
+  onMounted(() => {
+    let map = L.map('map').setView([52.09027905360301, 5.107968723830769], 7);
+
+    // ask location and zoom in on your location if accepted
+    navigator.geolocation.getCurrentPosition(
+      (position) => {map.setView([position.coords.latitude, position.coords.longitude], 12)}, 
+      (error) => console.warn(error.message)
+    );
+
+    // initializing mapp
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    // markerclusters
+    var markers = L.markerClusterGroup();
+
+    // adding all markers
+    LocationsNL.forEach(loc => {
+      let marker = L.marker(loc.latlng);
+      marker.bindPopup(`
+        <div class="relative w-full h-full flex flex-col items-center"> 
+          <img class="w-24 max-h-24 mb-4 object-contain" src="${loc.logo}"/>
+          <h2 class="text-xl font-bold mb-4 text-center">${loc.name}</h2>
+          <p class>${loc.desc}</p> 
+        </div>
+      `);
+      markers.addLayer(marker);
+    });
+
+    // adding markers and markerclusters
+    map.addLayer(markers);
+  });
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  app: {
+    head: {
+      charset: 'utf-8',
+      link: [
+        {rel: 'stylesheet', href: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css', integrity: "sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=", crossorigin: ""},
+        {rel: 'stylesheet', href: 'https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css'},
+        {rel: 'stylesheet', href: 'https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css'},
+      ],
+      script: [
+        {src: "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js", integrity: "sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=", crossorigin: ""},
+        {src: "https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js"}
+      ]
+    }
+  }
 })
